83f3d21e400f1c33f0e1d9abe48e34c9af6f7601
Loyio <i@loyio.me>
2021-04-15 15:27:41 +0800

    delete facenet and test mtcnn

--- automation_faceRecognition/face_boundary_camera.py
+++ automation_faceRecognition/face_boundary_camera.py
@@ -1,5 +1,27 @@
 import cv2
+import time
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from mtcnn import MTCNN
 
+detector = MTCNN()
 
+capture = cv2.VideoCapture(0)
+time.sleep(1)
+
+while(True):
+    ret, frame = capture.read()
+    frame = cv2.flip(frame,1)
+    frame = cv2.resize(frame, (600, 400))
+    boxes = detector.detect_faces(frame)
+    if boxes:
+        box = boxes[0]['box']
+        conf = boxes[0]['confidence']
+        x, y, w, h = box[0], box[1], box[2], box[3]
+
+        if conf > 0.5:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255), 1)
+    cv2.imshow("video", frame)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+cv2.destroyAllWindows()
